480234cbda80765e300c4a6c97ebd7b554cfa631
Wilbur W <wilburwei.2008@gmail.com>
2024-05-27 14:30:16 -0400

    Add impact page

--- app/impact/page.tsx
+++ app/impact/page.tsx
@@ -12,7 +12,7 @@ const Approach = () => {
       <div className="max-w-7xl flex flex-col justify-center p-1 md:px-10 mx-auto sm:pt-10 lg:flex-row lg:justify-between items-center">
 
         <h1 className="text-5xl font-bold leadi sm:text-6xl leading-snug">
-          Our<span className="text-rose-600 leading-snug"> Approach </span>
+          Our<span className="text-rose-600 leading-snug"> Impact </span>
 
         </h1>
       </div>
