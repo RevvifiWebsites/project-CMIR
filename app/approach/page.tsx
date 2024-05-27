d59bcb75187bb2adea74027c03d7deb9aeaf21cf
Wilbur W <wilburwei.2008@gmail.com>
2024-05-27 14:28:44 -0400

    Edit Navbar and create approach page

---
+++ app/approach/page.tsx
@@ -0,0 +1,22 @@
+"use client"
+
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
+
+import React, { useState, ChangeEvent, FormEvent } from "react";
+const Approach = () => {
+  return (
+    <>
+      <div className="max-w-7xl flex flex-col justify-center p-1 md:px-10 mx-auto sm:pt-10 lg:flex-row lg:justify-between items-center">
+
+        <h1 className="text-5xl font-bold leadi sm:text-6xl leading-snug">
+          Our               <span className="text-rose-600 leading-snug"> Approach </span>
+
+        </h1>
+      </div>
+    </>
+  )
+}
+export default Approach;
